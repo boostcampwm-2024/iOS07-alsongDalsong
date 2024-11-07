5b1cfefc622faf73e5bc2b1b4bf2c05b9a8e5f5b
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-11-07 14:44:37 +0900

    test: PR 머지 연습

--- alsongDalsong/ViewController.swift
+++ alsongDalsong/ViewController.swift
@@ -8,11 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(named: "fake"), for: .normal)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        button.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        button.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        button.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
 
