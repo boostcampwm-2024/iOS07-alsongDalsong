954a45855b9938cbf025f022b394b13d881e7f15
TLtlbo <js1436kt@gmail.com>
2024-11-11 23:57:57 +0900

    feat: 녹음 종료 기능 구현 및 테스트 코드 추가

--- ASAudioKit/ASAudioRecorder/ASAudioRecorder.swift
+++ ASAudioKit/ASAudioRecorder/ASAudioRecorder.swift
@@ -26,5 +26,8 @@ class ASAudioRecorder {
         return false
     }
     
-    func stopRecording() {}
+    func stopRecording() {
+        // 녹음 종료 시에 어디 저장되었는지 리턴해줄 필요가 있을 듯, 저장된 녹음파일을 network에 던져줄 필요가 있음.
+        audioRecorder?.stop()
+    }
 }
