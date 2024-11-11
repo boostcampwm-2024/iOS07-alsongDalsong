258c73be95689edec04acca18f39ab800c762ceb
moral-life <leeminam32@gmail.com>
2024-11-12 00:38:24 +0900

    feat: ASMusicAPI 구조체 생성, search 함수 및, ASSong 선언
    1. search를 Songs를 받아오는것, Song을 ASSong으로 변환하는 두가지 작업을 하는데 이를 분리하는 것이 좋을지
    2. 위 방식을 택했을 때 MusicKit 예시 앱 처럼 파도타기 (노래 -> 관련 앨범 -> 아티스트 -> 다른앨범 -> ....) 하는 기능을 잃게 될 것 같은데 괜찮은지

---
+++ alsongDalsong/ASMusicAPI.swift
@@ -0,0 +1,38 @@
+import MusicKit
+
+struct ASMusicAPI {
+    
+    ///  search 결과를 Assong으로 변환 하여 배열로 리턴하는 함수
+    ///  노래 검색화면에 기본으로 뜨는 row 개수가 6개이므로 별다른 지정 없으면 기본값 6으로 지정
+    func search(for text: String, _ maxCount: Int = 6) async -> [ASSong] {
+        
+            let status = await MusicAuthorization.request()
+            
+            switch status {
+            case .authorized:
+                do {
+                    var request = MusicCatalogSearchRequest(term: text, types: [Song.self])
+                    request.limit = maxCount
+                    request.offset = 1
+                    
+                    let result = try await request.response()
+                    let asSongs = result.songs.map { song in
+                        ASSong(title: song.title, artistName: song.artistName)
+                    }
+                    return asSongs
+                } catch {
+                    print(String(describing: error))
+                    return []
+                }
+            default:
+                print("Not authorized to access Apple Music")
+                return []
+            }
+    }
+}
+
+struct ASSong: Equatable {
+    let title: String
+    let artistName: String
+}
+
