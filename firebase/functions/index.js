f6aec373fc62e2cb8433f99011ffe095564ae8d9
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-11-13 01:29:20 +0900

    feat: API Deploy

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,12 +1,16 @@
 const { onRequest } = require('firebase-functions/v2/https');
 const admin = require('./FirebaseAdmin.js');
 const { createRoom } = require('./api/CreateRoom.js');
+const { joinRoom } = require('./api/JoinRoom.js');
 const { startGame } = require('./api/StartGame.js');
 const { uploadRecord } = require('./api/UploadRecord.js');
 const { onRecordAdded } = require('./trigger/onRecordAdded.js');
+const { onRoundEdited } = require('./trigger/onRoundEdited.js');
 
-// 방 생성 API
+// 방 관련 API
 exports.createRoom = createRoom;
+exports.joinRoom = joinRoom;
+exports.startGame = startGame;
 
 // GameStart API
 exports.startGame = startGame;
@@ -16,3 +20,6 @@ exports.uploadRecording = uploadRecord;
 
 // Record 추가 트리거
 exports.onRecordAdded = onRecordAdded;
+
+// Round 추가 트리거
+// exports.onRoundEdited = onRoundEdited;
