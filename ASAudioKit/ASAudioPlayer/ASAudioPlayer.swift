8d3e84e953befb49eab9caf2e34bda2984c8d7a8
TLtlbo <js1436kt@gmail.com>
2024-11-12 16:22:19 +0900

    feat: 재생 기능 구현

--- ASAudioKit/ASAudioPlayer/ASAudioPlayer.swift
+++ ASAudioKit/ASAudioPlayer/ASAudioPlayer.swift
@@ -1,5 +1,32 @@
 import Foundation
+import AVFoundation
 
-class ASAudioPlayer {
+public enum PlayType {
+    case allPlay
+    case play(time: Int)
+}
+
+public class ASAudioPlayer {
+    private var audioPlayer: AVAudioPlayer? = nil
     
+    public init() {}
+    
+    public func startPlaying(data: Data, option: PlayType) {
+        do {
+            audioPlayer = try AVAudioPlayer(data: data)
+            audioPlayer?.prepareToPlay()
+        } catch {
+            //TODO: 오디오 객체생성 실패 시 처리
+        }
+        
+        switch option {
+        case .allPlay:
+            audioPlayer?.play()
+        case .play(let time):
+            audioPlayer?.currentTime = 0
+            let timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(time), repeats: false) { [weak self] _ in
+                self?.audioPlayer?.stop()
+            }
+        }
+    }
 }
--- ASAudioKit/ASAudioRecorder/ASAudioRecorder.swift
+++ ASAudioKit/ASAudioRecorder/ASAudioRecorder.swift
@@ -1,9 +1,11 @@
 import AVFoundation
 
-class ASAudioRecorder {
+public class ASAudioRecorder {
     private var audioRecorder: AVAudioRecorder? = nil
     
-    func startRecording(url: URL) {
+    public init() {}
+    
+    public func startRecording(url: URL) {
         let settings = [
             AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
             AVSampleRateKey: 12000,
@@ -19,14 +21,14 @@ class ASAudioRecorder {
         }
     }
     
-    func isRecording() -> Bool {
+    public func isRecording() -> Bool {
         if let audioRecorder {
             return audioRecorder.isRecording
         }
         return false
     }
     
-    func stopRecording() -> Data? {
+    public func stopRecording() -> Data? {
         // 녹음 종료 시에 어디 저장되었는지 리턴해줄 필요가 있을 듯, 저장된 녹음파일을 network에 던져줄 필요가 있음.
         audioRecorder?.stop()
         
