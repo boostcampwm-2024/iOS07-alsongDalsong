85232b506d8c48c5bb3923ee4c0d0c93c4478c91
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-11-13 01:26:48 +0900

    feat: 게임을 시작 요청을 하는 API

--- firebase/functions/api/StartGame.js
+++ firebase/functions/api/StartGame.js
@@ -13,5 +13,38 @@ const admin = require('../FirebaseAdmin.js');
  * @returns Boolean
  */
 module.exports.startGame = onRequest({ region: 'asia-southeast1' }, async (req, res) => {
-  res.status(200).json({ message: 'Start Game' });
+  const { roomNumber, userId } = req.body;
+  const roomRef = admin.firestore().collection('rooms').doc(roomNumber);
+
+  try {
+    const roomSnapshot = await roomRef.get();
+    if (!roomSnapshot.exists) {
+      return res.status(404).json({ error: 'Room not found' });
+    }
+
+    const roomData = roomSnapshot.data();
+
+    // 요청한 유저가 방의 호스트인지 확인
+    if (roomData.host.id !== userId) {
+      return res.status(403).json({ error: 'Only the host can start the game' });
+    }
+
+    if (roomData.mode === 'humming') {
+      // TODO Player Order 설정
+      // TODO 게임 모드에 따른 라운드 설정 및 status 변경, records 초기화
+      await roomRef.update({
+        status: 'humming',
+        round: 1,
+        records: Array.from({ length: roomData.players.length }, () => ({ data: null })),
+      });
+
+      const updatedRoomData = (await roomRef.get()).data();
+      res.status(200).json(updatedRoomData);
+    } else {
+      res.status(400).json({ error: 'Invalid mode' });
+    }
+  } catch (error) {
+    logger.error('Error starting game:', error);
+    res.status(500).json({ error: 'Failed to start game' });
+  }
 });
