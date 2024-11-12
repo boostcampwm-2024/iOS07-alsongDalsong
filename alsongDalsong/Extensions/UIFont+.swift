e4fd4ce4cd3831d0acc6d42fd34c5c9e8b38d427
moral-life <leeminam32@gmail.com>
2024-11-12 14:20:54 +0900

    fix: 한칸 띄우기

--- alsongDalsong/Extensions/UIFont+.swift
+++ alsongDalsong/Extensions/UIFont+.swift
@@ -3,6 +3,7 @@ import UIKit
 enum FontName: String {
     case dohyeon = "Dohyeon-Regular"
 }
+
 extension UIFont {
     static func font(_ style: FontName, ofSize size: CGFloat) -> UIFont {
         guard let customFont = UIFont(name: style.rawValue, size: size) else {
