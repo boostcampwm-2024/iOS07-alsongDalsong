6c0bc0448133260a75ddb75a5ecebfdaeca04be9
TLtlbo <js1436kt@gmail.com>
2024-11-12 13:21:43 +0900

    refactor: README 작성 및 녹음 종료 시 Data타입을 리턴하게 수정

--- ASAudioKit/ASAudioRecorder/ASAudioRecorder.swift
+++ ASAudioKit/ASAudioRecorder/ASAudioRecorder.swift
@@ -5,17 +5,17 @@ class ASAudioRecorder {
     
     func startRecording(url: URL) {
         let settings = [
-            AVFormatIDKey : Int(kAudioFormatMPEG4AAC),
-            AVSampleRateKey : 12000,
-            AVNumberOfChannelsKey : 1,
-            AVEncoderAudioQualityKey : AVAudioQuality.high.rawValue
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
         ]
         
         do {
             audioRecorder = try AVAudioRecorder(url: url, settings: settings)
             audioRecorder?.record()
         } catch {
-            //
+            //TODO: AVAudioRecorder 객체 생성 실패 시에 대한 처리
         }
     }
     
@@ -26,8 +26,17 @@ class ASAudioRecorder {
         return false
     }
     
-    func stopRecording() {
+    func stopRecording() -> Data? {
         // 녹음 종료 시에 어디 저장되었는지 리턴해줄 필요가 있을 듯, 저장된 녹음파일을 network에 던져줄 필요가 있음.
         audioRecorder?.stop()
+        
+        guard let recordURL = audioRecorder?.url else {return nil}
+        
+        do {
+            let recordData = try Data(contentsOf: recordURL)
+            return recordData
+        } catch {
+            return nil
+        }
     }
 }
