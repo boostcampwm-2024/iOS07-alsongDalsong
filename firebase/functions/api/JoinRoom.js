b0d0cbe5fb6ca5b6f26a5259fe9526aadac46c3d
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-11-13 01:22:30 +0900

    feat: 방 참여를 요청하는 API 추가

--- firebase/functions/api/JoinRoom.js
+++ firebase/functions/api/JoinRoom.js
@@ -11,5 +11,50 @@ const admin = require('../FirebaseAdmin.js');
  * @returns playerData
  */
 module.exports.joinRoom = onRequest({ region: 'asia-southeast1' }, async (req, res) => {
-  res.status(200).json({ message: 'Join Room' });
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Only POST requests are accepted' });
+  }
+
+  const { roomNumber, userId } = req.body;
+  if (!roomNumber || !userId) {
+    return res.status(400).json({ error: 'Room number and user ID are required' });
+  }
+
+  const roomRef = firestore.collection('rooms').doc(roomNumber);
+  try {
+    const roomSnapshot = await roomRef.get();
+    if (!roomSnapshot.exists) {
+      return res.status(404).json({ error: 'Room not found' });
+    }
+
+    const roomData = roomSnapshot.data();
+    const playerExists = roomData.players.some((player) => player.id === userId);
+
+    if (playerExists) {
+      return res.status(400).json({ error: 'User already in the room' });
+    }
+
+    const userData = await getUserData(userId);
+    if (!userData) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const player = {
+      id: userData.id,
+      avatar: userData.avatar,
+      nickname: userData.nickname,
+      score: userData.score,
+      order: 0,
+    };
+
+    await roomRef.update({
+      players: FieldValue.arrayUnion(player),
+    });
+
+    const updatedRoomData = (await roomRef.get()).data();
+    res.status(200).json(updatedRoomData);
+  } catch (error) {
+    logger.error('Error joining room:', error);
+    res.status(500).json({ error: 'Failed to join room' });
+  }
 });
