32b2af1d5cded65c819047e08e143f9ff1588bd6
psangwon62 <psangwon62@gmail.com>
2024-11-13 16:01:17 +0900

    chore: SwiftLint 적용

--- ASAudioKit/ASAudioRecorder/ASAudioRecorder.swift
+++ ASAudioKit/ASAudioRecorder/ASAudioRecorder.swift
@@ -1,10 +1,10 @@
 import AVFoundation
 
 public final class ASAudioRecorder {
-    private var audioRecorder: AVAudioRecorder? = nil
-    
+    private var audioRecorder: AVAudioRecorder?
+
     public init() {}
-    
+
     public func startRecording(url: URL) {
         let settings = [
             AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
@@ -12,28 +12,28 @@ public final class ASAudioRecorder {
             AVNumberOfChannelsKey: 1,
             AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
         ]
-        
+
         do {
             audioRecorder = try AVAudioRecorder(url: url, settings: settings)
             audioRecorder?.record()
         } catch {
-            //TODO: AVAudioRecorder 객체 생성 실패 시에 대한 처리
+            // TODO: AVAudioRecorder 객체 생성 실패 시에 대한 처리
         }
     }
-    
+
     public func isRecording() -> Bool {
         if let audioRecorder {
             return audioRecorder.isRecording
         }
         return false
     }
-    
+
     public func stopRecording() -> Data? {
         // 녹음 종료 시에 어디 저장되었는지 리턴해줄 필요가 있을 듯, 저장된 녹음파일을 network에 던져줄 필요가 있음.
         audioRecorder?.stop()
-        
+
         guard let recordURL = audioRecorder?.url else {return nil}
-        
+
         do {
             let recordData = try Data(contentsOf: recordURL)
             return recordData
