743de683c65061ebae8cf9ab6bac298160b91635
moral-life <leeminam32@gmail.com>
2024-11-13 22:26:53 +0900

    fix: textSize도 변경 가능하도록 파라미터 추가

--- alsongDalsong/Components/UIKitComponents/ASButton.swift
+++ alsongDalsong/Components/UIKitComponents/ASButton.swift
@@ -1,7 +1,6 @@
 import UIKit
 
 class ASButton: UIButton {
-
     init(systemImageName: String = "", title: String, backgroundColor: UIColor) {
         super.init(frame: .zero)
         setConfiguration(systemImageName: systemImageName, title: title, backgroundColor: backgroundColor)
@@ -14,10 +13,10 @@ class ASButton: UIButton {
     
     /// 버튼의 UI 관련한 Configuration을 설정하는 메서드
     /// - Parameters:
-    ///   - systemImageName: SF Symbol 이미지를 삽입읍 원할 경우 "play.fill" 과 같이 systemName 입력. 입력 안할시 이미지 입력 안됨.
+    ///   - systemImageName: SF Symbol 이미지를 삽입을 원할 경우 "play.fill" 과 같이 systemName 입력. 입력 안할시 이미지 입력 안됨.
     ///   - title: 버튼에 쓰일 텍스트
     ///   - backgroundColor: UIColor 형태로 색깔 입력.  (ex.   .asYellow)
-    private func setConfiguration(systemImageName: String, title: String, backgroundColor: UIColor) {
+    private func setConfiguration(systemImageName: String, title: String, backgroundColor: UIColor, textSize: CGFloat = 32) {
         var config = UIButton.Configuration.gray()
         
         config.baseBackgroundColor = backgroundColor
@@ -41,13 +40,5 @@ class ASButton: UIButton {
         config.cornerStyle = .medium
         
         self.configuration = config
-    }
-    
-    /// 버튼에 지정된 그림자를 추가하는 메서드
-    private func setShadow() {
-        self.layer.shadowColor = UIColor.asShadow.cgColor
-        self.layer.shadowOpacity = 1
-        self.layer.shadowOffset = CGSize(width: 4, height: 4)
-        self.layer.shadowRadius = 0
     }
 }
