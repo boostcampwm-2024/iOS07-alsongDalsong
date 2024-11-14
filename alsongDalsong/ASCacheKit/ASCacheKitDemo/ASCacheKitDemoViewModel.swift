1d924be51b45e0be88764186fa6c69ed3b8b69be
psangwon62 <psangwon62@gmail.com>
2024-11-15 03:13:03 +0900

    feat: 이미지 다운로드 및 캐싱, 캐싱 삭제 구현, 토스트 구현

--- alsongDalsong/ASCacheKit/ASCacheKitDemo/ASCacheKitDemoViewModel.swift
+++ alsongDalsong/ASCacheKit/ASCacheKitDemo/ASCacheKitDemoViewModel.swift
@@ -1,3 +1,27 @@
+import ASCacheKit
 import Foundation
 
-class ASCacheKitDemoViewModel: ObservableObject {}
+class ASCacheKitDemoViewModel: ObservableObject {
+    private var cacheManager = ASCacheManager()
+    private let imageURL = URL(string: "https://picsum.photos/id/13/600/600")!
+    @Published var imageData: Data?
+
+    func loadCacheData(at cacheOption: CacheOption) {
+        Task {
+            imageData = await cacheManager.loadCache(from: imageURL, cacheOption: cacheOption)
+        }
+    }
+
+    func clearCache(at cacheOption: CacheOption) {
+        switch cacheOption {
+            case .onlyMemory:
+                cacheManager.clearMemoryCache()
+            case .onlyDisk:
+                cacheManager.clearDiskCache()
+            case .both:
+                cacheManager.clearMemoryCache()
+                cacheManager.clearDiskCache()
+            default: break
+        }
+    }
+}
