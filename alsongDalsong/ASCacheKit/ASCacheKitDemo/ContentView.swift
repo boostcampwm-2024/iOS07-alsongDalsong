29c16cfe7d79602221ed76b1d6947493a5523e3a
psangwon62 <psangwon62@gmail.com>
2024-11-15 02:14:36 +0900

    feat: 캐시 모듈 데모 앱 생성

---
+++ alsongDalsong/ASCacheKit/ASCacheKitDemo/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ASCacheKitDemoView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {}
+                .navigationTitle("ASCacheKit Demo")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    ASCacheKitDemoView()
+}
