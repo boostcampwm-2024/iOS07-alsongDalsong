4117d70b4d743bc3f084fe1ba759f599a6668486
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-11-14 17:19:54 +0900

    feat: 파이어베이스 Demo App 기본 설정

---
+++ alsongDalsong/ASNetworkKit/ASFirebaseDemo/Sources/SceneDelegate.swift
@@ -0,0 +1,17 @@
+import Firebase
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo _: UISceneSession,
+               options _: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        FirebaseApp.configure()
+        window = UIWindow(windowScene: windowScene)
+        let viewController = MainViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
