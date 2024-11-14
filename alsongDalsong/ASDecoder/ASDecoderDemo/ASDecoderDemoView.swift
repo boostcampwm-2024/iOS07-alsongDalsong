644a3c64c1e6383865cbf0a167da5ac826bf0b67
psangwon62 <psangwon62@gmail.com>
2024-11-15 01:48:38 +0900

    feat: 참고 데이터 뷰 추가

--- alsongDalsong/ASDecoder/ASDecoderDemo/ASDecoderDemoView.swift
+++ alsongDalsong/ASDecoder/ASDecoderDemo/ASDecoderDemoView.swift
@@ -7,27 +7,51 @@ struct ASDecoderDemoView: View {
         NavigationStack {
             VStack {
                 Group {
-                    if let userInfo = viewModel.userInfo {
-                        VStack(alignment: .leading, spacing: 10) {
+                    VStack(alignment: .leading) {
+                        if viewModel.jsonString == nil {
+                            titleText("디코딩할 자료 구조")
+                            Text(viewModel.modelString)
+                        }
+
+                        if let jsonString = viewModel.jsonString {
+                            titleText("원본 JSON")
+                            Text(jsonString)
+                        }
+                    }
+                }
+                .padding()
+                .foregroundStyle(Color.white)
+                .background(LinearGradient(colors: [.red, .purple, .yellow],
+                                           startPoint: .topLeading,
+                                           endPoint: .bottomTrailing))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+
+                Group {
+                    VStack(alignment: .leading) {
+                        if let userInfo = viewModel.userInfo {
+                            titleText("디코딩 정보")
                             Text("사용자 ID: \(userInfo.id)")
                             Text("사용자 이름: \(userInfo.userName)")
                             Text("아바타 URL: \(userInfo.userAvatarUrl.absoluteString)")
                             Text("생년월일: \(userInfo.userBirthDate, formatter: viewModel.customDateFormatter)")
                             Text("상태: \(userInfo.userStatus)")
+                        } else if let errorMessage = viewModel.errorMessage {
+                            titleText("디코딩 정보")
+                            Text(errorMessage)
+                                .foregroundColor(.red)
+                        } else {
+                            Text("디코딩 시나리오를 선택하세요")
                         }
-                    } else if let errorMessage = viewModel.errorMessage {
-                        Text(errorMessage)
-                            .foregroundColor(.red)
-                    } else {
-                        Text("디코딩 시나리오를 선택하세요")
                     }
                 }
                 .padding()
                 .multilineTextAlignment(.center)
-                .background(Color(uiColor: .systemGroupedBackground))
+                .background(LinearGradient(colors: [.cyan, .green, .yellow],
+                                           startPoint: .topLeading,
+                                           endPoint: .bottomTrailing))
                 .clipShape(RoundedRectangle(cornerRadius: 10))
-                
-                VStack(spacing: 10) {
+
+                VStack {
                     Button("정상 JSON 디코딩") {
                         Task {
                             await viewModel.loadUserInfo(from: .correct)
@@ -59,6 +83,14 @@ struct ASDecoderDemoView: View {
             .navigationTitle("Decoding Demo")
             .navigationBarTitleDisplayMode(.inline)
         }
+    }
+
+    @ViewBuilder
+    func titleText(_ string: String) -> some View {
+        Text(string)
+            .font(.title2)
+            .foregroundStyle(Color.white)
+            .bold()
     }
 }
 
--- alsongDalsong/ASDecoder/ASDecoderDemo/ASDecoderDemoViewModel.swift
+++ alsongDalsong/ASDecoder/ASDecoderDemo/ASDecoderDemoViewModel.swift
@@ -5,15 +5,26 @@ import Foundation
 class ASDecoderDemoViewModel: ObservableObject {
     @Published var userInfo: UserInfo?
     @Published var errorMessage: String?
-    @Published var scenario: JSONDataScenarios?
-    
+    @Published var jsonString: String?
+
     var customDateFormatter: DateFormatter {
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
         return formatter
     }
 
+    let modelString = """
+            struct UserInfo {
+                let id: Int
+                let userName: String
+                let userAvatarUrl: URL
+                let userBirthDate: Date
+                let userStatus: UserStatus
+            }
+        """
+
     func loadUserInfo(from scenario: Scenarios) async {
+        jsonString = scenario.string
         let result: Result<Data, Error> = .success(scenario.data)
 
         do {
@@ -27,5 +38,6 @@ class ASDecoderDemoViewModel: ObservableObject {
     func reset() {
         userInfo = nil
         errorMessage = nil
+        jsonString = nil
     }
 }
