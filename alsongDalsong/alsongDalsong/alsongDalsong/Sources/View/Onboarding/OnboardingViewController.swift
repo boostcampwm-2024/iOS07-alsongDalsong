c3ac91340c948c0409f9bb9498b39fa07dc01272
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-11-17 19:57:53 +0900

    refactor: Constant 값 추가

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/View/Onboarding/OnboardingViewController.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/View/Onboarding/OnboardingViewController.swift
@@ -1,57 +1,31 @@
 import UIKit
 
 final class OnboardingViewController: UIViewController {
-    private var logoImageView: UIImageView!
-    private var createRoomButton: ASButton!
-    private var joinRoomButton: ASButton!
-    private var avatarView: ASAvatarCircleView!
-    private var nickNameTextField: ASTextField!
-    private var nickNamePanel: ASPanel!
-    private var nickNameRefreshButton: ASRefreshButton!
+    private var logoImageView = UIImageView(image: UIImage(named: Constants.logoImageName))
+    private var createRoomButton = ASButton(title: Constants.craeteButtonTitle, backgroundColor: .asYellow)
+    private var joinRoomButton = ASButton(title: Constants.joinButtonTitle, backgroundColor: .asMint)
+    private var avatarView = ASAvatarCircleView(image: UIImage(named: "mojojojo") ?? UIImage.fake)
+    private var nickNameTextField = ASTextField(placeholder: "도덕적인 삶")
+    private var nickNamePanel = ASPanel(title: Constants.nickNameTitle, titleAlign: .left, titleSize: 24)
+    private var nickNameRefreshButton = ASRefreshButton(size: 28)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .asLightGray
-        
         setupUI()
         setupLayout()
         setAction()
     }
     
     private func setupUI() {
-        //현재는 하드코딩으로 들어가지만 이런 문구들을 모아놓는 다른 파일이 있으면 좋을 것 같음 (혹시나 모를 외국어 지원 등...)
-        createRoomButton = ASButton(title: "방 생성하기!", backgroundColor: .asYellow)
-        self.view.addSubview(createRoomButton)
-        
-        joinRoomButton = ASButton(title: "방 참가하기!", backgroundColor: .asMint)
-        self.view.addSubview(joinRoomButton)
-        
-        logoImageView = UIImageView(image: UIImage(named: "logo"))
-        self.view.addSubview(logoImageView)
-        
-        avatarView = ASAvatarCircleView(image: UIImage(named: "mojojojo") ?? UIImage.fake)
-        self.view.addSubview(avatarView)
-        
-        nickNamePanel = ASPanel(title: "닉네임", titleAlign: .left, titleSize: 24)
-        self.view.addSubview(nickNamePanel)
-        
-        //placeholder는 추후 랜덤 생성된 닉네임으로 바뀌어야 함
-        nickNameTextField = ASTextField(placeholder: "도덕적인 삶")
-        nickNamePanel.addSubview(nickNameTextField)
-        
-        nickNameRefreshButton = ASRefreshButton(size: 28)
-        self.view.addSubview(nickNameRefreshButton)
+        view.backgroundColor = .asLightGray
+        [createRoomButton, joinRoomButton, logoImageView, avatarView, nickNamePanel, nickNameTextField, nickNameRefreshButton].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
     }
     
     private func setupLayout() {
         let safeArea = self.view.safeAreaLayoutGuide
-        createRoomButton.translatesAutoresizingMaskIntoConstraints = false
-        joinRoomButton.translatesAutoresizingMaskIntoConstraints = false
-        logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        avatarView.translatesAutoresizingMaskIntoConstraints = false
-        nickNamePanel.translatesAutoresizingMaskIntoConstraints = false
-        nickNameTextField.translatesAutoresizingMaskIntoConstraints = false
-        nickNameRefreshButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             joinRoomButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24),
@@ -96,15 +70,13 @@ final class OnboardingViewController: UIViewController {
         joinRoomButton.addAction(
             UIAction { [weak self] _ in
                 let joinAlert = ASAlertController(
-                    titleText: "게임 입장 코드를 입력하세요",
-                    doneButtonTitle: "입장",
-                    cancelButtonTitle: "취소",
-                    textFieldPlaceholder: "#A3B4C5"
+                    titleText: Constants.joinAlertTitle,
+                    doneButtonTitle: Constants.doneAlertButtonTitle,
+                    cancelButtonTitle: Constants.cancelAlertButtonTitle,
+                    textFieldPlaceholder: Constants.roomNumberPlaceholder
                 )
-                joinAlert.cancelButtonCompletion = { [weak self] in
-                    print("취소")
-                }
                 joinAlert.doneButtonCompletion = { [weak self] in
+                    // TODO: 방 참가 로직 추가
                     print("입력된 텍스트: \(joinAlert.text)")
                 }
                 self?.present(joinAlert, animated: true, completion: nil)
@@ -113,4 +85,16 @@ final class OnboardingViewController: UIViewController {
     }
 }
 
-extension L
+extension OnboardingViewController {
+    enum Constants {
+        static let craeteButtonTitle = "방 생성하기!"
+        static let joinButtonTitle = "방 참가하기!"
+        static let joinAlertTitle = "게임 입장 코드를 입력하세요"
+        static let nickNameTitle = "닉네임"
+        static let doneAlertButtonTitle = "완료"
+        static let cancelAlertButtonTitle = "취소"
+        static let roomNumberPlaceholder = "#000000"
+        
+        static let logoImageName = "logo"
+    }
+}
