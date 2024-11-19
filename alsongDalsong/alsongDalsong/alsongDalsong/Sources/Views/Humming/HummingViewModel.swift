9fed22d213d00629265d6131277a3c23ab255b3b
psangwon62 <psangwon62@gmail.com>
2024-11-19 17:25:44 +0900

    refactor: hummingViewModel 접근제어자 수정

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Humming/HummingViewModel.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Humming/HummingViewModel.swift
@@ -3,7 +3,7 @@ import ASRepository
 import Combine
 import Foundation
 
-private final class HummingViewModel {
+final class HummingViewModel {
     @Published public private(set) var dueTime: Date?
     @Published public private(set) var round: UInt8?
     @Published public private(set) var status: Status?
