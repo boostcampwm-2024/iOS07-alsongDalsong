336daf7b72bcb6f94b8f7f570beb0410121757eb
moral-life <leeminam32@gmail.com>
2024-11-21 18:45:42 +0900

    feat: MusicRepository 추가
    - 음악 프리뷰 뿐만 아니라 레코드도 활용 가능

---
+++ alsongDalsong/ASRepository/ASRepository/Repositories/MusicRepository.swift
@@ -0,0 +1,44 @@
+import ASNetworkKit
+import Combine
+import Foundation
+
+public final class MusicRepository: MusicRepositoryProtocol {
+    // TODO: - Container로 주입
+    private let firebaseManager: ASFirebaseManager
+    private let networkManager: ASNetworkManager
+    
+    public init (
+        firebaseManager: ASFirebaseManager,
+        networkManager: ASNetworkManager
+    ) {
+        self.firebaseManager = firebaseManager
+        self.networkManager = networkManager
+    }
+    
+    public func getMusicUrls() -> Future<[URL], Error> {
+        Future { promise in
+            Task {
+                do {
+                    let urls = try await self.firebaseManager.getAvatarUrls()
+                    promise(.success(urls))
+                } catch {
+                    promise(.failure(error))
+                }
+            }
+        }
+    }
+    
+    public func getMusicData(url: URL) -> Future<Data?, Error> {
+        Future { promise in
+            Task {
+                do {
+                    guard let endpoint = ResourceEndpoint(url: url) else { return promise(.failure(ASNetworkErrors.urlError)) }
+                    let data = try await self.networkManager.sendRequest(to: endpoint)
+                    promise(.success(data))
+                } catch {
+                    promise(.failure(error))
+                }
+            }
+        }
+    }
+}
