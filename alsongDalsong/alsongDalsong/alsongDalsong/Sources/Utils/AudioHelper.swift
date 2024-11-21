134da2485b4025f78b4ef5003912c7d03e3bfb93
psangwon62 <psangwon62@gmail.com>
2024-11-21 20:14:32 +0900

    feat: AudioHelper 녹음기능에 visualize 기능 추가

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Utils/AudioHelper.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Utils/AudioHelper.swift
@@ -1,12 +1,20 @@
 import ASAudioKit
+import Combine
 import Foundation
+
+extension AnyPublisher: @unchecked @retroactive Sendable {}
 
 actor AudioHelper {
     static let shared = AudioHelper()
     private var recorder: ASAudioRecorder?
     private var player: ASAudioPlayer?
-
+    private var timer: Timer?
+    private let amplitudeSubject = PassthroughSubject<Float, Never>()
     private init() {}
+
+    func amplitudePubisher() -> AnyPublisher<Float, Never> {
+        return amplitudeSubject.eraseToAnyPublisher()
+    }
 
     func isRecording() async -> Bool {
         guard let recorder else { return false }
@@ -22,14 +30,47 @@ actor AudioHelper {
         makeRecorder()
         let tempURL = makeURL()
         await recorder?.startRecording(url: tempURL)
+        visualize()
         return await withCheckedContinuation { continuation in
             DispatchQueue.main.asyncAfter(deadline: .now() + 6) { [weak self] in
                 Task {
                     let recordedData = await self?.stopRecording()
+                    await self?.removeTimer()
                     continuation.resume(returning: recordedData)
                 }
             }
         }
+    }
+
+    private func removeTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+
+    private func visualize() {
+        Task { [weak self] in
+            guard let self else { return }
+            await self.setTimer()
+        }
+    }
+
+    private func setTimer() {
+        timer = Timer(timeInterval: 0.08, repeats: true) { [weak self] _ in
+            Task {
+                print("calculate amplitude")
+                await self?.calculateRecorderAmplitude()
+            }
+        }
+        RunLoop.main.add(timer!, forMode: .common)
+    }
+    
+    private func calculateRecorderAmplitude() async {
+        await recorder?.updateMeters()
+        guard let averagePower = await recorder?.getAveragePower() else { return }
+        let newAmplitude = 1.1 * pow(10.0, averagePower / 20.0)
+        let clampedAmplitude = min(max(newAmplitude, 0), 1)
+        print("clampedAmplitude")
+        amplitudeSubject.send(clampedAmplitude)
     }
 
     func startPlaying(file: Data?, playType: PlayType = .full) async {
