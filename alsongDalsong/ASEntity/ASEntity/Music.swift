d5ff7abecbf85fb4aaf9b58af631a5730dd45179
psangwon62 <psangwon62@gmail.com>
2024-11-24 03:02:02 +0900

    refactor: Music 구조체 url로 변경

--- alsongDalsong/ASEntity/ASEntity/Music.swift
+++ alsongDalsong/ASEntity/ASEntity/Music.swift
@@ -4,6 +4,8 @@ public struct Music: Codable, Equatable {
     public var id: UUID?
     public var title: String?
     public var artist: String?
+    public var artworkUrl: URL?
+    public var previewUrl: URL?
     public var lyrics: String?
 
     public init() {}
@@ -11,5 +13,12 @@ public struct Music: Codable, Equatable {
     public init(title: String, artist: String) {
         self.title = title
         self.artist = artist
+    }
+    
+    public init(title: String, artist: String, artworkUrl: URL?, previewUrl: URL?) {
+        self.title = title
+        self.artist = artist
+        self.artworkUrl = artworkUrl
+        self.previewUrl = previewUrl
     }
 }
