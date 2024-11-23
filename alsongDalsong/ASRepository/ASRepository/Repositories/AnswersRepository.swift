4d74178b8dfdf155e55e155ecabf55ecfba97f93
psangwon62 <psangwon62@gmail.com>
2024-11-24 03:03:39 +0900

    feat: answersrepo에서 내가 선택한 곡을 가져오는 함수 추가

--- alsongDalsong/ASRepository/ASRepository/Repositories/AnswersRepository.swift
+++ alsongDalsong/ASRepository/ASRepository/Repositories/AnswersRepository.swift
@@ -15,4 +15,16 @@ public final class AnswersRepository: AnswersRepositoryProtocol {
             .compactMap { $0 }
             .eraseToAnyPublisher()
     }
+    
+    public func getMyAnswer() -> AnyPublisher<Answer?, Never> {
+        mainRepository.answers
+            .receive(on: DispatchQueue.main)
+            .compactMap(\.self)
+            .flatMap { answers in
+                // TODO: - myId를 (저장해 두었다가 또는 가져와서) 필터링 필요
+                return Just(answers.first/* { $0.player?.id == "myId"}*/)
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
 }
