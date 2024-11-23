de01683da49956dc5d823e8506a02ab4003af892
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-11-24 00:28:28 +0900

    feat:방 나갈 때 alert 보여주기

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Lobby/LobbyViewController.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Lobby/LobbyViewController.swift
@@ -87,10 +87,20 @@ final class LobbyViewController: UIViewController {
     }
     
     private func setAction() {
-        backButton.addAction(UIAction { [weak self] _ in
-            self?.viewmodel.leaveRoom()
-            self?.navigationController?.popViewController(animated: true)
-        }, for: .touchUpInside)
+        backButton.addAction(
+            UIAction { [weak self] _ in
+                let leaveAlert = ASAlertController2(
+                    titleText: "정말로 나가시겠습니까?",
+                    doneButtonTitle: "나가기",
+                    cancelButtonTitle: "취소"
+                )
+                leaveAlert.doneButtonCompletion = { [weak self] in
+                    self?.viewmodel.leaveRoom()
+                    self?.navigationController?.popViewController(animated: true)
+                }
+                self?.present(leaveAlert, animated: true, completion: nil)
+            },
+            for: .touchUpInside)
         
         inviteButton.addAction(UIAction { [weak self] _ in
             guard let roomNumber = self?.viewmodel.roomNumber else { return }
