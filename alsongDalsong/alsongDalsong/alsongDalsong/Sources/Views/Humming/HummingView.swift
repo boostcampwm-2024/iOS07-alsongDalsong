524aa264ae467386f19aedd1705f3ef881bcb2ff
psangwon62 <psangwon62@gmail.com>
2024-11-24 19:49:27 +0900

    feat: musicPanel playbutton 클로저 제거

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Humming/HummingView.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Humming/HummingView.swift
@@ -35,9 +35,6 @@ final class HummingViewController: UIViewController {
         musicPanel.bind(to: vm.$music)
         hummingPanel.bind(to: vm.$recorderAmplitude)
         submitButton.bind(to: vm.$humming)
-        musicPanel.onPlayButtonTapped { [weak self] isPlaying in
-            self?.vm.togglePlayPause(of: .preview, isPlaying: isPlaying)
-        }
         hummingPanel.onPlayButtonTapped = { [weak self] isPlaying in
             self?.vm.togglePlayPause(of: .humming, isPlaying: isPlaying)
         }
