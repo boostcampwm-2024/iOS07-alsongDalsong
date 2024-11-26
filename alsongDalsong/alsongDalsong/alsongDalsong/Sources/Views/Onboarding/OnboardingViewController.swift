f2d19e21669c64f0d13a6c1343e1116c915c18c4
psangwon62 <psangwon62@gmail.com>
2024-11-26 18:08:19 +0900

    chore: alert present 관련 extension 파일 생성

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Onboarding/OnboardingViewController.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Onboarding/OnboardingViewController.swift
@@ -251,12 +251,3 @@ extension OnboardingViewController {
     }
 }
 
-extension UIViewController {
-    func presentAlert(_ viewcontrollerToPresent: ASAlertController) {
-        present(viewcontrollerToPresent, animated: true, completion: nil)
-    }
-
-    func presentLoadingView(_ viewcontrollerToPresent: ASAlertController) {
-        present(viewcontrollerToPresent, animated: true, completion: nil)
-    }
-}
