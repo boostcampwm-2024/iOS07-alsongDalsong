15d31587218456f7861f5d007e447dc6a3c16c8a
psangwon62 <psangwon62@gmail.com>
2024-11-28 20:09:10 +0900

    feat: Answer 갯수 가져오는 함수 작성

--- alsongDalsong/ASRepository/ASRepository/Repositories/AnswersRepository.swift
+++ alsongDalsong/ASRepository/ASRepository/Repositories/AnswersRepository.swift
@@ -12,40 +12,48 @@ public final class AnswersRepository: AnswersRepositoryProtocol {
         self.mainRepository = mainRepository
         self.networkManager = networkManager
     }
-    
+
     public func getAnswers() -> AnyPublisher<[Answer], Never> {
         mainRepository.answers
             .receive(on: DispatchQueue.main)
             .compactMap { $0 }
             .eraseToAnyPublisher()
     }
-    
+
+    public func getAnswersCount() -> AnyPublisher<Int, Never> {
+        mainRepository.answers
+            .receive(on: DispatchQueue.main)
+            .compactMap { $0 }
+            .map { $0.count }
+            .eraseToAnyPublisher()
+    }
+
     public func getMyAnswer() -> AnyPublisher<Answer?, Never> {
         guard let myId = mainRepository.myId else {
             return Just(nil).eraseToAnyPublisher()
         }
-        
+
         return mainRepository.answers
             .receive(on: DispatchQueue.main)
             .compactMap(\.self)
             .flatMap { answers in
-                Just(answers.first { $0.player?.id == myId} )
+                Just(answers.first { $0.player?.id == myId })
                     .eraseToAnyPublisher()
             }
             .eraseToAnyPublisher()
     }
-    
+
     public func submitMusic(answer: ASEntity.Music) async throws -> Bool {
         let queryItems = [URLQueryItem(name: "userId", value: ASFirebaseAuth.myID),
                           URLQueryItem(name: "roomNumber", value: mainRepository.number.value)]
         let endPoint = FirebaseEndpoint(path: .submitMusic, method: .post)
             .update(\.queryItems, with: queryItems)
-        
+
         let body = try ASEncoder.encode(answer)
         try print(JSONSerialization.jsonObject(with: body, options: []))
-        
+
         let response = try await networkManager.sendRequest(to: endPoint, type: .json, body: body, option: .none)
-        
+
         let responseDict = try ASDecoder.decode([String: String].self, from: response)
         return !responseDict.isEmpty
     }
