8a44e930d9f4563876d4825e69ee651283db42b5
psangwon62 <psangwon62@gmail.com>
2024-11-29 04:14:45 +0900

    feat: 로비 뷰 리팩토링
    - 방 코드 크기 줄이고 중간으로 이동, 색상 변경
    - alert 코드 통일

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Lobby/LobbyViewController.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Lobby/LobbyViewController.swift
@@ -11,20 +11,20 @@ final class LobbyViewController: UIViewController {
     let inviteButton = ASButton()
     let startButton = ASButton()
     private var hostingController: UIHostingController<LobbyView>?
-    
+
     let viewmodel: LobbyViewModel
     private var cancellables: Set<AnyCancellable> = []
-    
+
     init(lobbyViewModel: LobbyViewModel) {
-        self.viewmodel = lobbyViewModel
+        viewmodel = lobbyViewModel
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     @available(*, unavailable)
-    required init?(coder: NSCoder) {
+    required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -32,7 +32,7 @@ final class LobbyViewController: UIViewController {
         setAction()
         bindToComponents()
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         if viewmodel.isLeaveRoom {
@@ -40,14 +40,14 @@ final class LobbyViewController: UIViewController {
             hostingController = nil
         }
     }
-    
+
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         if viewmodel.isLeaveRoom {
             viewmodel.leaveRoom()
         }
     }
-    
+
     private func bindToComponents() {
         viewmodel.$roomNumber
             .receive(on: DispatchQueue.main)
@@ -55,18 +55,19 @@ final class LobbyViewController: UIViewController {
                 self?.codeLabel.text = "#\(roomNumber)"
             }
             .store(in: &cancellables)
-        
+
         viewmodel.$isHost
             .receive(on: DispatchQueue.main)
             .sink { [weak self] isHost in
                 self?.startButton.isEnabled = isHost
             }
             .store(in: &cancellables)
-        
+
         viewmodel.$canBeginGame
             .receive(on: DispatchQueue.main)
             .sink { [weak self] canBeginGame in
                 if !canBeginGame {
+                    self?.startButton.updateButton(.waitStart)
                     self?.startButton.updateButton(.disabled)
                 } else {
                     self?.startButton.updateButton(.startGame)
@@ -75,48 +76,50 @@ final class LobbyViewController: UIViewController {
             }
             .store(in: &cancellables)
     }
-    
+
     private func setupUI() {
         view.backgroundColor = .asLightGray
         topNavigationView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         backButton.translatesAutoresizingMaskIntoConstraints = false
         var configuration = UIButton.Configuration.plain()
-        
+
         configuration.image = UIImage(systemName: "rectangle.portrait.and.arrow.forward")?
             .withRenderingMode(.alwaysTemplate)
             .applyingSymbolConfiguration(.init(pointSize: 24, weight: .medium))?
             .rotate(radians: .pi)
         backButton.configuration = configuration
         backButton.tintColor = .asBlack
-        
+
         codeLabel.translatesAutoresizingMaskIntoConstraints = false
-        codeLabel.font = .font(forTextStyle: .largeTitle)
-        codeLabel.textColor = .gray
-        codeLabel.textAlignment = .right
-        
+        codeLabel.font = .font(forTextStyle: .title1)
+        codeLabel.textColor = .label
+        codeLabel.textAlignment = .center
+
         inviteButton.setConfiguration(systemImageName: "link", title: "초대하기!", backgroundColor: .asYellow)
         inviteButton.translatesAutoresizingMaskIntoConstraints = false
-        
+
         startButton.setConfiguration(systemImageName: "play.fill", title: "시작하기!", backgroundColor: .asMint)
         startButton.translatesAutoresizingMaskIntoConstraints = false
     }
-    
+
     private func setAction() {
         backButton.addAction(
             UIAction { [weak self] _ in
-                let leaveAlert = ASAlertController(
+                let alert = ASAlertController(
+                    style: .default,
                     titleText: .leaveRoom,
                     doneButtonTitle: .leave,
-                    cancelButtonTitle: .cancel)
-                { [weak self] _ in
+                    cancelButtonTitle: .cancel
+                ) { [weak self] _ in
                     self?.viewmodel.leaveRoom()
                     self?.navigationController?.popViewController(animated: true)
                 }
-                self?.present(leaveAlert, animated: true, completion: nil)
+                self?.presentAlert(alert)
             },
-            for: .touchUpInside)
-        
+            for: .touchUpInside
+        )
+
         inviteButton.addAction(UIAction { [weak self] _ in
             guard let roomNumber = self?.viewmodel.roomNumber else { return }
             if let url = URL(string: "alsongDalsong://invite/?roomnumber=\(roomNumber)") {
@@ -125,27 +128,28 @@ final class LobbyViewController: UIViewController {
                 self?.present(activityViewController, animated: true, completion: nil)
             }
         }, for: .touchUpInside)
-        
+
         startButton.addAction(
             UIAction { [weak self] _ in
                 guard let playerCount = self?.viewmodel.players.count else { return }
                 if playerCount < 3 {
-                    let nojamAlert = ASAlertController(
+                    let alert = ASAlertController(
                         style: .default,
                         titleText: .needMorePlayer,
                         doneButtonTitle: .keep,
-                        cancelButtonTitle: .cancel)
-                    { _ in
+                        cancelButtonTitle: .cancel
+                    ) { [weak self] _ in
                         self?.showStartGameLoading()
                     }
-                    self?.present(nojamAlert, animated: true, completion: nil)
+                    self?.presentAlert(alert)
                 } else {
                     self?.showStartGameLoading()
                 }
             },
-            for: .touchUpInside)
+            for: .touchUpInside
+        )
     }
-    
+
     private func setupLayout() {
         let lobbyView = UIHostingController(rootView: LobbyView(viewModel: viewmodel))
         hostingController = lobbyView
@@ -156,41 +160,40 @@ final class LobbyViewController: UIViewController {
         view.addSubview(topNavigationView)
         topNavigationView.addSubview(codeLabel)
         topNavigationView.addSubview(backButton)
-        
+
         let safeArea = view.safeAreaLayoutGuide
-        
+
         NSLayoutConstraint.activate([
             topNavigationView.topAnchor.constraint(equalTo: safeArea.topAnchor),
             topNavigationView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
             topNavigationView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
             topNavigationView.heightAnchor.constraint(equalToConstant: 44),
-            
+
             backButton.leadingAnchor.constraint(equalTo: topNavigationView.leadingAnchor, constant: 16),
             backButton.centerYAnchor.constraint(equalTo: topNavigationView.centerYAnchor),
             backButton.heightAnchor.constraint(equalToConstant: 44),
             backButton.widthAnchor.constraint(equalToConstant: 44),
-            
-            codeLabel.leadingAnchor.constraint(equalTo: backButton.trailingAnchor, constant: 16),
-            codeLabel.trailingAnchor.constraint(equalTo: topNavigationView.trailingAnchor, constant: -16),
+
             codeLabel.centerYAnchor.constraint(equalTo: topNavigationView.centerYAnchor),
-            
+            codeLabel.centerXAnchor.constraint(equalTo: topNavigationView.centerXAnchor),
+
             lobbyView.view.topAnchor.constraint(equalTo: topNavigationView.bottomAnchor),
             lobbyView.view.bottomAnchor.constraint(equalTo: inviteButton.topAnchor, constant: -20),
             lobbyView.view.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
             lobbyView.view.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
-            
+
             inviteButton.bottomAnchor.constraint(equalTo: startButton.topAnchor, constant: -25),
             inviteButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24),
             inviteButton.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -24),
             inviteButton.heightAnchor.constraint(equalToConstant: 64),
-            
+
             startButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -25),
             startButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24),
             startButton.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -24),
-            startButton.heightAnchor.constraint(equalToConstant: 64)
+            startButton.heightAnchor.constraint(equalToConstant: 64),
         ])
     }
-    
+
     private func gameStart() async throws {
         do {
             try await viewmodel.gameStart()
@@ -211,10 +214,11 @@ extension LobbyViewController {
             },
             errorCompletion: { [weak self] error in
                 self?.showStartGameFailed(error)
-            })
+            }
+        )
         presentLoadingView(alert)
     }
-    
+
     func showStartGameFailed(_ error: Error) {
         let alert = ASAlertController(titleText: .error(error))
         presentAlert(alert)
