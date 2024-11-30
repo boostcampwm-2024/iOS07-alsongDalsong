6da9ca188c4864ee3552d03135d9a041686e2964
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-11-30 14:48:48 +0900

    fix: Refresh Button 다크모드 적용

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Components/UIKitComponents/ASRefreshButton.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Components/UIKitComponents/ASRefreshButton.swift
@@ -1,44 +1,31 @@
-//
-//  ASRefreshButton.swift
-//  alsongDalsong
-//
-//  Created by Minha Lee on 11/14/24.
-//
-
 import UIKit
 
 class ASRefreshButton: UIButton {
+    
     init(size: CGFloat) {
         super.init(frame: .zero)
         setConfiguration(size: size)
     }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     private func setConfiguration(size: CGFloat) {
         var config = UIButton.Configuration.gray()
 
         config.baseBackgroundColor = .asSystem
-        config.baseForegroundColor = .asShadow
-        
+        config.baseForegroundColor = .asBlack
+
         config.imagePlacement = .all
         config.image = UIImage(systemName: "arrow.clockwise")
-//        config.imagePadding = 10
         config.cornerStyle = .capsule
-//        config.background.strokeColor = .black
-//        config.background.strokeWidth = 3
-        
+
         let imageConfig = UIImage.SymbolConfiguration(pointSize: size, weight: .bold)
         config.preferredSymbolConfigurationForImage = imageConfig
-        
-//        var titleAttr = AttributedString.init(title)
-//        titleAttr.font = UIFont.font(.dohyeon, ofSize: 32)
-//        config.attributedTitle = titleAttr
-        
-//        config.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 12)
         config.cornerStyle = .capsule
-        
-        self.configuration = config
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+
+        configuration = config
     }
 }
