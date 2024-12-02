30fd55fbc493b8c9693ae816da90caa6279ade00
moral-life <leeminam32@gmail.com>
2024-12-03 00:14:11 +0900

    feat: ASMusicKit에  randomSong() 함수 추가, search @MainActor 삭제

--- alsongDalsong/ASMusicKit/ASMusicKit/ASMusicAPI.swift
+++ alsongDalsong/ASMusicKit/ASMusicKit/ASMusicAPI.swift
@@ -4,12 +4,12 @@ import MusicKit
 
 public struct ASMusicAPI {
     public init() {}
+
     /// MusicKit을 통해 Apple Music의 노래를 검색합니다.
     /// - Parameters:
     ///   - text: 검색 요청을 보낼 검색어
     ///   - maxCount: 검색해서 찾아올 음악의 갯수 기본값 설정은 25
     /// - Returns: Music의 배열
-    @MainActor
     public func search(for text: String, _ maxCount: Int = 10, _ offset: Int = 0) async throws -> [Music] {
         let status = await MusicAuthorization.request()
         switch status {
@@ -61,11 +61,45 @@ public struct ASMusicAPI {
                 throw ASMusicError.notAuthorized
         }
     }
+
+    public func randomSong(from playlistId: String) async throws -> ASEntity.Music {
+        let status = await MusicAuthorization.request()
+        switch status {
+            case .authorized:
+                do {
+                    let request = MusicCatalogResourceRequest<MusicKit.Playlist>(matching: \.id, equalTo: MusicItemID(rawValue: playlistId))
+                    let playlistResponse = try await request.response()
+                    let playlist = playlistResponse.items.first!
+
+                    let playlistWithTrack = try await playlist.with([.tracks])
+                    guard let tracks = playlistWithTrack.tracks else {
+                        throw ASMusicError.playListHasNoSongs
+                    }
+
+                    if let song = tracks.randomElement() {
+                        return ASEntity.Music(
+                            id: song.id.rawValue,
+                            title: song.title,
+                            artist: song.artistName,
+                            artworkUrl: song.artwork?.url(width: 300, height: 300),
+                            previewUrl: song.previewAssets?.first?.url,
+                            artworkBackgroundColor: song.artwork?.backgroundColor?.toHex()
+                        )
+                    }
+                } catch {
+                    throw ASMusicError.playListHasNoSongs
+                }
+            default:
+                throw ASMusicError.notAuthorized
+        }
+        return ASEntity.Music(id: "nil", title: nil, artist: nil, artworkUrl: nil, previewUrl: nil, artworkBackgroundColor: nil)
+    }
 }
 
 public enum ASMusicError: Error, LocalizedError {
     case notAuthorized
     case searchError
+    case playListHasNoSongs
 
     public var errorDescription: String? {
         switch self {
@@ -73,6 +107,8 @@ public enum ASMusicError: Error, LocalizedError {
                 "애플 뮤직에 접근하는 권한이 없습니다."
             case .searchError:
                 "노래 검색 중 오류가 발생했습니다."
+            case .playListHasNoSongs:
+                "플레이리스트에 노래가 없습니다."
         }
     }
 }
